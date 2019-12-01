1c73506a14cf831c49378dbcec43e20813ffc4fd
Shin Negishi <snegishi@users.noreply.github.com>
2019-12-01 22:46:36 +0900

    implemented label and button

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
 
+    func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
