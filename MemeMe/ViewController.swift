1cf0a0be0fd06fd85ba911bdd526f4f7734c9238
Shin Negishi <snegishi@users.noreply.github.com>
2019-12-02 23:52:55 +0900

    changed GUI from programatic implementation to Storyboard

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,30 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label: UILabel!
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        // label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        view.addSubview(label)
-        self.label = label
-        
-        // button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
     }
 
-    @objc func incrementCount() {
+    @IBAction func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
     }
